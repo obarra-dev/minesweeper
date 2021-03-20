910f4e694b3750ed8f5034c9bf99032321017d67
obarra-dev <barra_omar@hotmail.com>
2021-03-20 16:07:49 -0300

    test github actions

--- model.go
+++ model.go
@@ -3,13 +3,13 @@ package minesweeper
 // StateTile is a enum, it's represents the state of the tile.
 type StateTile int
 
-// All possible states of the tile.
+
 const (
 	StateTileCovered  StateTile = 1
 	StateTileClear    StateTile = 2
 	StateTileFlagged  StateTile = 3
 	StateTileNumberd  StateTile = 4
-	StateTileExploted StateTile = 5,
+	StateTileExploted StateTile = 5
 )
 
 // StateGame is a enum, it's represents the state of the game.
