1101d3136e0e7f12e4573504f5cb916cea25ec7c
obarra-dev <barra_omar@hotmail.com>
2021-05-22 17:28:57 -0300

    adjust test

--- engine_test.go
+++ engine_test.go
@@ -3,12 +3,18 @@ package minesweeper_test
 import (
 	"fmt"
 	"github.com/obarra-dev/minesweeper"
+	"reflect"
 	"testing"
 )
 
 func Test_New(t *testing.T) {
 	t.Run("board 3x3", func(t *testing.T) {
-		game := minesweeper.New(3, 3, minesweeper.GenerateMines(0, 0, 0))
+		mines := []minesweeper.Mine{
+			{R: 0, C: 1},
+			{R: 1, C: 1},
+			{R: 1, C: 0},
+		}
+		game := minesweeper.New(3, 3, mines)
 
 		if len(game.Board) != game.Rows || game.Rows != 3 {
 			t.Errorf("expect %d got %d", 3, len(game.Board))
@@ -16,6 +22,14 @@ func Test_New(t *testing.T) {
 
 		if len(game.Board[0]) != game.Columns || game.Columns != 3 {
 			t.Errorf("expect %d got %d", 3, len(game.Board[0]))
+		}
+
+		expect := [][]minesweeper.Tile{
+			{{minesweeper.StateTileCovered, 0, 0, 3, false, 1}, {minesweeper.StateTileCovered, 0, 1, 2, true, 2}, {minesweeper.StateTileCovered, 0, 2, 2, false, 3}},
+			{{minesweeper.StateTileCovered, 1, 0, 2, true, 4}, {minesweeper.StateTileCovered, 1, 1, 2, true, 5}, {minesweeper.StateTileCovered, 1, 2, 2, false, 6}},
+			{{minesweeper.StateTileCovered, 2, 0, 2, false, 7}, {minesweeper.StateTileCovered, 2, 1, 2, false, 8}, {minesweeper.StateTileCovered, 2, 2, 1, false, 9}}}
+		if !reflect.DeepEqual(expect, game.Board) {
+			t.Errorf("expect %+v got %+v", expect, game.Board)
 		}
 	})
 }
@@ -49,7 +63,7 @@ func Test_Play(t *testing.T) {
 		mines := []minesweeper.Mine{{R: 1, C: 1}}
 		game := minesweeper.New(3, 3, mines)
 
-		tests := []struct {
+		caseTests := []struct {
 			r, c   int
 			move   minesweeper.TypeMove
 			expect minesweeper.StateGame
@@ -63,100 +77,85 @@ func Test_Play(t *testing.T) {
 			{2, 1, minesweeper.TypeMoveClean, minesweeper.StateGameRunning},
 			{2, 2, minesweeper.TypeMoveClean, minesweeper.StateGameWon},
 		}
-		for i, d := range tests {
-			got := game.Play(d.r, d.c, d.move).State
-			if got != d.expect {
+		for i, ct := range caseTests {
+			got := game.Play(ct.r, ct.c, ct.move).State
+			if got != ct.expect {
 				t.Errorf("Test[%d]: game.Play(%d,%d,%d) expect %d, got %d",
-					i, d.r, d.c, d.move, d.expect, got)
+					i, ct.r, ct.c, ct.move, ct.expect, got)
+			}
+		}
+	})
+
+	t.Run("when flag and revert flag", func(t *testing.T) {
+		type expect struct {
+			flagAmount int
+			gameState  minesweeper.StateGame
+			tileState  minesweeper.StateTile
+		}
+
+		caseTests := []struct {
+			r, c   int
+			move   minesweeper.TypeMove
+			expect expect
+		}{
+			{1, 1, minesweeper.TypeMoveFlag, expect{1, minesweeper.StateGameRunning, minesweeper.StateTileFlagged}},
+			{1, 1, minesweeper.TypeMoveRevertFlag, expect{0, minesweeper.StateGameRunning, minesweeper.StateTileFlagged}},
+		}
+
+		game := minesweeper.New(3, 3, []minesweeper.Mine{})
+		for i, ct := range caseTests {
+			gameCopy := game.Play(ct.r, ct.c, ct.move)
+			got := expect{
+				flagAmount: gameCopy.FlagAmount,
+				gameState:  gameCopy.State,
+				tileState:  gameCopy.Board[0][0].State,
+			}
+			if got != ct.expect {
+				t.Errorf("Test[%d]: game.Play(%d,%d,%d) expect %d, got %d",
+					i, ct.r, ct.c, ct.move, ct.expect, got)
 			}
 		}
 	})
 }
 
-/**
+func Test_RevealEmptyAdjacentTiles(t *testing.T) {
+	t.Run("3x3", func(t *testing.T) {
+		mines := []minesweeper.Mine{{R: 1, C: 1}}
+		game := minesweeper.New(3, 3, mines)
 
-func TestSetUpMines(t *testing.T) {
-	minedPointTile := [][2]int{{0, 1}, {1, 1}, {1, 0}}
-	game := minesweeper.New(3, 3, minedPointTile)
-	game.ShowBoard()
+		game.RevealEmptyAdjacentTiles(0, 0)
 
-	expected := [][]minesweeper.Tile{
-		{{minesweeper.StateTileCovered, 0, 0, 3, false, 1}, minesweeper.Tile{minesweeper.StateTileCovered, 0, 1, 2, true, 2}, minesweeper.Tile{minesweeper.StateTileCovered, 0, 2, 2, false, 3}},
-		{minesweeper.Tile{minesweeper.StateTileCovered, 1, 0, 2, true, 4}, minesweeper.Tile{minesweeper.StateTileCovered, 1, 1, 2, true, 5}, minesweeper.Tile{minesweeper.StateTileCovered, 1, 2, 2, false, 6}},
-		{minesweeper.Tile{minesweeper.StateTileCovered, 2, 0, 2, false, 7}, minesweeper.Tile{minesweeper.StateTileCovered, 2, 1, 2, false, 8}, minesweeper.Tile{minesweeper.StateTileCovered, 2, 2, 1, false, 9}}}
+		expect := [][]minesweeper.StateTile{
+			{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileCovered},
+			{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileCovered},
+			{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileCovered}}
 
-	if !reflect.DeepEqual(expected, game.Board) {
-		t.Error("Error", game.Board)
-	}
-}
-
-
-func TestRevealEmptyAdjacentTiles3x3(t *testing.T) {
-	minedPointTile := [][2]int{{1, 1}}
-	game := minesweeper.New(3, 3, minedPointTile)
-
-	game.RevealEmptyAdjacentTiles(0, 0)
-	game.ShowBoard()
-
-	expected := [][]minesweeper.StateTile{
-		{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileCovered},
-		{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileCovered},
-		{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileCovered}}
-
-	if !reflect.DeepEqual(expected, game.GetStates()) {
-		t.Error("Error", game.GetStates())
-	}
-}
-
-func TestRevealEmptyAdjacentTiles3x8(t *testing.T) {
-	minedPointTile := [][2]int{{1, 1}}
-	game := minesweeper.New(3, 8, minedPointTile)
-
-	game.RevealEmptyAdjacentTiles(0, 5)
-	game.ShowBoard()
-
-	expected := [][]minesweeper.StateTile{
-		{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileNumbered, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear},
-		{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileNumbered, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear},
-		{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileNumbered, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear},
-	}
-	if !reflect.DeepEqual(expected, game.GetStates()) {
-		t.Error("Error", game.GetStates())
-	}
-}
-
-func TestPlayMoveWhenFlag(t *testing.T) {
-	game := minesweeper.New(3, 3, [][2]int{})
-
-	gameCopy := game.Play(1, 1, minesweeper.TypeMoveFlag)
-
-	if gameCopy.State != minesweeper.StateGameRunning || game.FlagAmount != 1 || gameCopy.Board[0][0].State != minesweeper.StateTileFlagged {
-		t.Error("Error", game.FlagAmount, gameCopy)
-	}
-}
-
-func TestPlayMoveWhenRevertTheFlag(t *testing.T) {
-	game := minesweeper.New(3, 3, [][2]int{})
-
-	gameCopy := game.Play(1, 1, minesweeper.TypeMoveFlag)
-	gameCopy = game.Play(1, 1, minesweeper.TypeMoveRevertFlag)
-
-	if gameCopy.State != minesweeper.StateGameRunning || gameCopy.FlagAmount != 0 {
-		t.Error("Error", gameCopy, game.FlagAmount)
-	}
-}
-*/
-
-func showBoard(g minesweeper.Game) {
-	for i := 0; i < g.Rows; i++ {
-		for j := 0; j < g.Columns; j++ {
-			fmt.Print(g.Board[i][j], " ")
+		got := getStateTiles(*game)
+		if !reflect.DeepEqual(expect, got) {
+			t.Errorf("expect %+v got %+v", expect, game.Board)
 		}
-		fmt.Println()
-	}
+	})
+
+	t.Run("3x8", func(t *testing.T) {
+		mines := []minesweeper.Mine{{R: 1, C: 1}}
+		game := minesweeper.New(3, 8, mines)
+
+		game.RevealEmptyAdjacentTiles(0, 5)
+
+		expect := [][]minesweeper.StateTile{
+			{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileNumbered, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear},
+			{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileNumbered, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear},
+			{minesweeper.StateTileCovered, minesweeper.StateTileCovered, minesweeper.StateTileNumbered, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear, minesweeper.StateTileClear},
+		}
+
+		got := getStateTiles(*game)
+		if !reflect.DeepEqual(expect, got) {
+			t.Errorf("expect %+v got %+v", expect, game.Board)
+		}
+	})
 }
 
-func GetStates(g minesweeper.Game) [][]minesweeper.StateTile {
+func getStateTiles(g minesweeper.Game) [][]minesweeper.StateTile {
 	states := make([][]minesweeper.StateTile, g.Rows)
 
 	for i := 0; i < g.Rows; i++ {
@@ -166,4 +165,13 @@ func GetStates(g minesweeper.Game) [][]minesweeper.StateTile {
 		}
 	}
 	return states
+}
+
+func showBoard(g minesweeper.Game) {
+	for i := 0; i < g.Rows; i++ {
+		for j := 0; j < g.Columns; j++ {
+			fmt.Print(g.Board[i][j], " ")
+		}
+		fmt.Println()
+	}
 }
